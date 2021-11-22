45c01af508f6eb74c7c16c691a87ed99693d4a99
Eric Thomas <ethomasosu@gmail.com>
2021-11-22 15:42:48 -0500

    make exception response

--- src/main/java/com/psb/controller/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/psb/controller/RestResponseEntityExceptionHandler.java
@@ -1,11 +1,10 @@
 package com.psb.controller;
 
-import org.springframework.http.HttpHeaders;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.psb.exception.SpotifyClientException;
@@ -14,16 +13,28 @@ import com.psb.exception.SpotifyClientUnauthorizedException;
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-	@ExceptionHandler(value = { SpotifyClientUnauthorizedException.class })
-	protected ResponseEntity<Object> handleWebClientConflict(SpotifyClientUnauthorizedException ex,
-			WebRequest request) {
-		String bodyOfResponse = "Error calling spotify api. " + ex.getMessage();
-		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
-	}
+//	@ExceptionHandler(value = { SpotifyClientUnauthorizedException.class })
+//	protected ResponseEntity<Object> handleWebClientConflict(SpotifyClientUnauthorizedException ex,
+//			WebRequest request) {
+//		String bodyOfResponse = "Error calling spotify api. " + ex.getMessage();
+//		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
+//	}
+//
+//	@ExceptionHandler(value = { SpotifyClientException.class })
+//	protected ResponseEntity<Object> handleSpotifyException(SpotifyClientException ex, WebRequest request) {
+//		String bodyOfResponse = "Error calling spotify api. " + ex.getMessage();
+//		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.SERVICE_UNAVAILABLE, request);
+//	}
 
+	@ExceptionHandler(value = { SpotifyClientUnauthorizedException.class })
+	public ResponseEntity<Object> handleUnauthorizedException(SpotifyClientUnauthorizedException e) {
+		ExceptionResponse res = new ExceptionResponse(e.getMessage(), HttpStatus.UNAUTHORIZED);
+		return new ResponseEntity<>(res, HttpStatus.UNAUTHORIZED);
+	}
+	
 	@ExceptionHandler(value = { SpotifyClientException.class })
-	protected ResponseEntity<Object> handleSpotifyException(SpotifyClientException ex, WebRequest request) {
-		String bodyOfResponse = "Error calling spotify api. " + ex.getMessage();
-		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.SERVICE_UNAVAILABLE, request);
+	public ResponseEntity<Object> handleClientException(SpotifyClientUnauthorizedException e) {
+		ExceptionResponse res = new ExceptionResponse(e.getMessage(), HttpStatus.UNAUTHORIZED);
+		return new ResponseEntity<>(res, HttpStatus.SERVICE_UNAVAILABLE);
 	}
 }
