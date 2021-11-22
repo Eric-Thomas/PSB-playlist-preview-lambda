5b21e5e2ab7364dba1386db38d01e6d7289540ba
Eric Thomas <ethomasosu@gmail.com>
2021-11-22 15:53:21 -0500

    remove commented code

--- src/main/java/com/psb/controller/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/psb/controller/RestResponseEntityExceptionHandler.java
@@ -13,19 +13,6 @@ import com.psb.exception.SpotifyClientUnauthorizedException;
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-//	@ExceptionHandler(value = { SpotifyClientUnauthorizedException.class })
-//	protected ResponseEntity<Object> handleWebClientConflict(SpotifyClientUnauthorizedException ex,
-//			WebRequest request) {
-//		String bodyOfResponse = "Error calling spotify api. " + ex.getMessage();
-//		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
-//	}
-//
-//	@ExceptionHandler(value = { SpotifyClientException.class })
-//	protected ResponseEntity<Object> handleSpotifyException(SpotifyClientException ex, WebRequest request) {
-//		String bodyOfResponse = "Error calling spotify api. " + ex.getMessage();
-//		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.SERVICE_UNAVAILABLE, request);
-//	}
-
 	@ExceptionHandler(value = { SpotifyClientUnauthorizedException.class })
 	public ResponseEntity<Object> handleUnauthorizedException(SpotifyClientUnauthorizedException e) {
 		ExceptionResponse res = new ExceptionResponse(e.getMessage(), HttpStatus.UNAUTHORIZED);
